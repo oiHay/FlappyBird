6f5f0e929a753c661e256796b00676cc533d10b4
Haykal <ferramentas@haykal.com.br>
2025-09-01 22:28:12 -0300

    FlappyBird

--- Assets/Scripts/Scenes/MainMenu/MainMenuEvents.cs
+++ Assets/Scripts/Scenes/MainMenu/MainMenuEvents.cs
@@ -1,27 +1,60 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class MainMenuEvents : MonoBehaviour
 {
     private UIDocument document;
 
-    private Button btn;
+    private VisualElement settingsPanel;
+
+    private Button btnStart;
+    private Button btnSettings;
+    private Button btnCloseSettings;
+
 
     private void Awake()
     {
         document = GetComponent<UIDocument>();
 
-        btn = document.rootVisualElement.Q("StartGameButton") as Button;
-        btn.RegisterCallback<ClickEvent>(OnPlayGameClick);
+        settingsPanel = document.rootVisualElement.Q("SettingsPanel");
+
+        btnStart = document.rootVisualElement.Q("StartGameButton") as Button;
+        btnStart.RegisterCallback<ClickEvent>(OnPlayGameClick);
+
+        btnSettings = document.rootVisualElement.Q("Settings") as Button;
+        btnSettings.RegisterCallback<ClickEvent>(OnOpeningSettings);
+
+        btnCloseSettings = document.rootVisualElement.Q("QuitSettings") as Button;
+        btnCloseSettings.RegisterCallback<ClickEvent>(OnClosingSettings);
     }
 
     private void OnDisable()
     {
-        btn.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+        btnStart.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+        btnSettings.UnregisterCallback<ClickEvent>(OnOpeningSettings);
+        btnCloseSettings.UnregisterCallback<ClickEvent>(OnClosingSettings);
     }
 
     private void OnPlayGameClick(ClickEvent evt)
     {
-        print("Start clicked");
+        LoadNextLevel();
+    }
+
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void OnOpeningSettings(ClickEvent evt)
+    {
+        if (settingsPanel != null)
+            settingsPanel.style.display = DisplayStyle.Flex;
+    }
+
+    private void OnClosingSettings(ClickEvent evt)
+    {
+        settingsPanel.style.display = DisplayStyle.None;
     }
 }
