2343a18b04656c7f33a7f422ebfc7a35534d0760
Haykal <ferramentas@haykal.com.br>
2025-09-13 12:37:19 -0300

    Flappy Bird

--- Assets/Scripts/Scenes/MainMenu/MainMenuEvents.cs
+++ Assets/Scripts/Scenes/MainMenu/MainMenuEvents.cs
@@ -7,10 +7,18 @@ public class MainMenuEvents : MonoBehaviour
     private UIDocument document;
 
     private VisualElement settingsPanel;
+    private VisualElement deathPanel;
 
     private Button btnStart;
     private Button btnSettings;
     private Button btnCloseSettings;
+
+    private Button Death_RetryBtn;
+    private Button Death_SettingsBtn;
+    private Button Death_MainMenuBtn;
+
+
+    private Button btnTest;
 
 
     private void Awake()
@@ -18,6 +26,7 @@ public class MainMenuEvents : MonoBehaviour
         document = GetComponent<UIDocument>();
 
         settingsPanel = document.rootVisualElement.Q("SettingsPanel");
+        deathPanel = document.rootVisualElement.Q("DeathPanel");
 
         btnStart = document.rootVisualElement.Q("StartGameButton") as Button;
         btnStart.RegisterCallback<ClickEvent>(OnPlayGameClick);
@@ -27,6 +36,18 @@ public class MainMenuEvents : MonoBehaviour
 
         btnCloseSettings = document.rootVisualElement.Q("QuitSettings") as Button;
         btnCloseSettings.RegisterCallback<ClickEvent>(OnClosingSettings);
+
+        Death_RetryBtn = document.rootVisualElement.Q("TryBtn") as Button;
+        Death_RetryBtn.RegisterCallback<ClickEvent>(OnTrying);
+
+        Death_SettingsBtn = document.rootVisualElement.Q("DeathSettingsBtn") as Button;
+        Death_SettingsBtn.RegisterCallback<ClickEvent>(OnOpeningSettings);
+
+        Death_MainMenuBtn = document.rootVisualElement.Q("MainMenuBtn") as Button;
+        Death_MainMenuBtn.RegisterCallback<ClickEvent>(BacktoMain);
+
+        btnTest = document.rootVisualElement.Q("testBtn") as Button;
+        btnTest.RegisterCallback<ClickEvent>(OnDying);
     }
 
     private void OnDisable()
@@ -56,5 +77,22 @@ public class MainMenuEvents : MonoBehaviour
     private void OnClosingSettings(ClickEvent evt)
     {
         settingsPanel.style.display = DisplayStyle.None;
+    }
+
+    private void OnDying(ClickEvent evt)
+    {
+        if (deathPanel != null)
+            deathPanel.style.display = DisplayStyle.Flex;
+    }
+
+    private void OnTrying(ClickEvent evt)
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
+    }
+
+    private void BacktoMain(ClickEvent evt)
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
